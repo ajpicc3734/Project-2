292b5e05da0f464fb58817193b7793dd2af055e2
Cbannerman <cbannerman21@gmail.com>
2022-06-10 12:28:02 -0400

    added record model code

---
+++ models/record.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequilize');
+const sequelize = require('../config/connection');
+
+class Record extends Model {}
+
+Record.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          artist: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          filename: {
+              type: DataTypes.STRING,
+              allowNull: false,
+          },
+          description: {
+              type: DataTypes.STRING,
+              allowNull: false,
+          },
+          user_id: {
+              type: DataTypes.STRING,
+              references: {
+                  model: 'user',
+                  key: 'id',
+              },
+          },
+
+
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'record',
+}
+);
+
+module.exports = Record;
