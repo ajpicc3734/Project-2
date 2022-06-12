9071e0b0d12c52d6e2e4bfb49bcee59c22f67804
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-06-11 20:30:59 -0400

    for pull

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+    app.listen(PORT, () => console.log("Now listening"));
 });
