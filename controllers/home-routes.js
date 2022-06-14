98f1fecca7b2719b1d6e878967add0c24c508a9b
johnc13b <100248387+johnc13b@users.noreply.github.com>
2022-06-14 16:42:37 -0400

    signup

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -102,12 +102,21 @@ router.get("/use", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-  //   if (req.session.loggedIn) {
-  //     res.redirect("/");
-  //     return;
-  //   }
+  // if (req.session.loggedIn) {
+  //   res.redirect("/");
+  //   return;
+  // }
 
   res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  // if (req.session.loggedIn) {
+  //   res.redirect("/");
+  //   return;
+  // }
+
+  res.render("signup");
 });
 
 module.exports = router;
