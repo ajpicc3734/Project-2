dacb7dc9523c6765c5859835469310b07f4e318d
Cbannerman <cbannerman21@gmail.com>
2022-06-13 21:07:29 -0400

    added in cloudinary routes

---
+++ models/Images.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Image extends Sequelize.Model {}
+
+Image.init(
+    {
+        id: {
+            type: Sequelize.DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        imageName: {
+            type: Sequelize.DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        modelName: "Image",
+        freezeTableName: true,
+    }
+);
+
+module.exports = Image;
