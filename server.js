ca94836c3d99fbf44f968dc930ca1c0deed7af7e
ajpicc3734 <99152798+ajpicc3734@users.noreply.github.com>
2022-06-17 13:51:24 -0400

    fixed merge conflicts and updated login

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 const path = require("path");
 const express = require("express");
-const session = require('express-session');
+const session = require("express-session");
 const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 const routes = require("./controllers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log("Now listening"));
 });
 
 // const path = require('path');
 // const express = require('express');
 // const session = require('express-session');
 // const exphbs = require('express-handlebars');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // const routes = require('./controllers');
 // const sequelize = require('./config/connection');
@@ -32,18 +45,6 @@ sequelize.sync({ force: false }).then(() => {
 
 // const app = express();
 // const PORT = process.env.PORT || 3001;
-
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize,
-//     }),
-// };
-
-// app.use(session(sess));
 
 // const hbs = exphbs.create({ helpers });
 
